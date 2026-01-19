1aa4e3285cee64a8a30f49fb442ca538e890cdfe
narayanbehera14 <beheranarayancs222330@gmail.com>
2026-01-20 01:19:33 +0530

    fix image filename case for vercel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -2,9 +2,9 @@ import { Swiper, SwiperSlide } from "swiper/react"
 import { Autoplay } from "swiper/modules"
 import "swiper/css"
 
-import l1 from "../images/logo-1.png"
-import l2 from "../images/logo-2.png"
-import l3 from "../images/logo.png"
+import l1 from "../images/Logo-1.png"
+import l2 from "../images/Logo-2.png"
+import l3 from "../images/Logo.png"
 
 const logos = [l1, l2, l3]
 
